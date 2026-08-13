1080ba8815ba4627185849fba15b7c78870a68d3
jc6036 <jc6036@gmail.com>
2026-08-13 14:04:58 -0400

    Initial interpreter

--- AstPrinter.cs
+++ AstPrinter.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace my_jlox
 {
+    // Implementation of "Visitor" pattern
+    // Operations on expressions all inherit from the Operate interface
+    // This enforces each operation to have an implementation for each expression type
+    // The operation class is called via an operation method, here it is Print
+    // It then calls the expression abstract method to pick a type for the operation
+    // Whichever sub type the call is routed to, has its own call back to the Operate implementer where it
+    // calls its own flavor of the operation
+    // So, operation -> visit/pick the correct expr type through type system -> Type -> Call back to given Operate implementer type to Do Thing For My Expr Type
+    //
+    // New Operation behaviors will be implemented via new Operate implementers which implement a behavior for each expr sub type
+    // with minimal need to edit the expr subtypes themselves
+    // New expr types are harder to add, as the base Operate interface and all implementers will need updated with new behavior
+    // once the expr subtype is added, but that's the tradeoff
     public class AstPrinter : Operate<string>
     {
         public string print(Expr expr)
@@ -30,7 +44,10 @@ namespace my_jlox
 
         public string opLiteral(Literal expr) // Literal, and so on
         {
-            return expr.value.ToString() ?? "nil";
+            if (expr.value != null)
+                return $"{expr.value}";
+            else
+                return "nil";
         }
 
         private string parenthesize(string name, params Expr[] exprs)
---
+++ Interpreter.cs
@@ -0,0 +1,98 @@
+﻿using System.Reflection.Metadata;
+
+namespace my_jlox
+{
+    public class Interpreter : Operate<object>
+    {
+        public object interpret(Expr expr)
+        {
+            return expr.pickForOp(this);
+        }
+
+        public object opLiteral(Literal expr)
+        {
+            return expr.value ?? "nil";
+        }
+
+        public object opGrouping(Grouping expr)
+        {
+            return evaluate(expr.expression);
+        }
+
+        public object? opUnary(Unary expr)
+        {
+            object right = evaluate(expr.right);
+
+            switch(expr.oprtr.type)
+            {
+                case TokenType.MINUS:
+                    return -(float)right;
+                case TokenType.BANG:
+                    return !isTruthy(right);
+            }
+
+            return null;
+        }
+
+        public object? opBinary(Binary expr)
+        {
+            object left = evaluate(expr.left);
+            object right = evaluate(expr.right);
+
+            switch(expr.oprtr.type)
+            {
+                case TokenType.GREATER:
+                    return (float)left > (float)right;
+                case TokenType.GREATER_EQUAL:
+                    return (float)left >= (float)right;
+                case TokenType.LESS:
+                    return (float)left < (float)right;
+                case TokenType.LESS_EQUAL:
+                    return (float)left <= (float)right;
+
+                case TokenType.BANG_EQUAL:
+                    return !isEqual(left, right); // Note we can check equality on any type
+                case TokenType.EQUAL_EQUAL:
+                    return isEqual(left, right);
+
+                case TokenType.MINUS:
+                    return (float)left - (float)right;
+                case TokenType.SLASH:
+                    return (float)left / (float)right;
+                case TokenType.STAR:
+                    return (float)left * (float)right;
+                case TokenType.PLUS:
+                    if(left.GetType() == typeof(float) && right.GetType() == typeof(float))
+                        return (float)left + (float)right;
+
+                    if (left.GetType() == typeof(string) && right.GetType() == typeof(string))
+                        return (string)left + (string)right;
+
+                    break;
+            }
+
+            return null;
+        }
+
+        private object evaluate(Expr expr)
+        {
+            return expr.pickForOp(this);
+        }
+
+        private bool isTruthy(object? val)
+        {
+            // 0 doesn't come back false which is crazy to me, but the book designs Lox this way...may revisit later
+            if (val == null) return false;
+            if (val.GetType() == typeof(bool)) return (bool)val;
+            return true;
+        }
+
+        private bool isEqual(object a, object b)
+        {
+            if (a == null && b == null) return true;
+            if (a == null) return false;
+
+            return a.Equals(b); // Primarily really leaning on C#'s equality checker here
+        }
+    }
+}
